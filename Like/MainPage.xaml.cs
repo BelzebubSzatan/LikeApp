293ef5290bc4d58d214661d77dcb343691828be8
BelzebubSzatan <kacpermatlag@gmail.com>
2023-12-06 10:22:04 +0100

    LikeCounter

---
+++ Like/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Like
+{
+    public partial class MainPage : ContentPage
+    {
+        private uint likeCounter = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void LikeButtonClick(object sender,EventArgs args)
+        {
+            likeCounter++;
+            Likes.Text = likeCounter.ToString() + " Likes";
+        }
+        public void DisLikeButtonClick(object sender, EventArgs args)
+        {
+            likeCounter = likeCounter <= 0 ? 0 : likeCounter--;
+            Likes.Text = likeCounter.ToString()+" Likes";
+        }
+    }
+}
